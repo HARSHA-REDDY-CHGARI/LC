f77b40ec7da64fb8d725d95a24a0a180192ad95f
HARSHA VARDHAN REDDY CHINNAKKA GARI <harsha.cg786@gmail.com>
2025-02-26 23:59:23 +0530

    Time: 1 ms (99.53%) | Memory: 57.5 MB (8.84%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+       
         int currsum=nums[0];
         int sofarsum=nums[0];
+       
         for(int i=1;i<nums.length;i++)
         {
            currsum =Math.max(nums[i],nums[i]+currsum);
             sofarsum=Math.max(currsum,sofarsum);
         }
+        
         return sofarsum;
         
     }
